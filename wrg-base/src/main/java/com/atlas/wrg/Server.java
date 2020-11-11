6e283dbdff862684ca2dfb313f6124b0b437bdf3
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-11-11 14:27:24 -0500

    Update RestServerFactory definition.

--- wrg-base/src/main/java/com/atlas/wrg/Server.java
+++ wrg-base/src/main/java/com/atlas/wrg/Server.java
@@ -2,14 +2,14 @@ package com.atlas.wrg;
 
 import java.net.URI;
 
+import com.atlas.shared.rest.RestServerFactory;
 import com.atlas.shared.rest.RestService;
-import com.atlas.shared.rest.ServerFactory;
 import com.atlas.shared.rest.UriBuilder;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 public class Server {
    public static void main(String[] args) {
       URI uri = UriBuilder.host(RestService.WORLD_REGISTRY).uri();
-      final HttpServer server = ServerFactory.create(uri, "com.atlas.wrg.rest");
+      final HttpServer server = RestServerFactory.create(uri, "com.atlas.wrg.rest");
    }
 }
