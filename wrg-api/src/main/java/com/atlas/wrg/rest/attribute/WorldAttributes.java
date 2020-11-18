c93cb314a9f40406e1ca1d7c41e51ffb2a96a4a3
Justin Tumidanski <jtumidanski@gmail.com>
2020-11-17 21:29:04 -0500

    Correct bug with World attributes.

--- wrg-api/src/main/java/com/atlas/wrg/rest/attribute/WorldAttributes.java
+++ wrg-api/src/main/java/com/atlas/wrg/rest/attribute/WorldAttributes.java
@@ -2,6 +2,6 @@ package com.atlas.wrg.rest.attribute;
 
 import rest.AttributeResult;
 
-public record WorldAttributes(String name, int flag, String message, String eventMessage, String recommended,
+public record WorldAttributes(String name, int flag, String message, String eventMessage, boolean recommended,
                               String recommendedMessage, int capacityStatus) implements AttributeResult {
 }
