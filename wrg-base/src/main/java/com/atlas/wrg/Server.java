fb03fbd04f7c7e5cd93ab2ef655a634ace686df1
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-11-18 15:42:23 -0500

    Remove unused variable. Correct issue with batch script.

--- wrg-base/src/main/java/com/atlas/wrg/Server.java
+++ wrg-base/src/main/java/com/atlas/wrg/Server.java
@@ -5,11 +5,10 @@ import java.net.URI;
 import com.atlas.shared.rest.RestServerFactory;
 import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
-import org.glassfish.grizzly.http.server.HttpServer;
 
 public class Server {
    public static void main(String[] args) {
       URI uri = UriBuilder.host(RestService.WORLD_REGISTRY).uri();
-      final HttpServer server = RestServerFactory.create(uri, "com.atlas.wrg.rest");
+      RestServerFactory.create(uri, "com.atlas.wrg.rest");
    }
 }
