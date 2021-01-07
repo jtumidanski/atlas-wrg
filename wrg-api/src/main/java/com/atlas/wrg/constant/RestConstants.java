6323c0e39eeeb5da48acc374d63e0abdb2c7d6f3
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-07 11:39:15 -0500

    Add constant for accessing rest service.

---
+++ wrg-api/src/main/java/com/atlas/wrg/constant/RestConstants.java
@@ -0,0 +1,8 @@
+package com.atlas.wrg.constant;
+
+public final class RestConstants {
+   private RestConstants() {
+   }
+
+   public static final String SERVICE = "wrg";
+}
--- wrg-base/src/main/java/com/atlas/wrg/Server.java
+++ wrg-base/src/main/java/com/atlas/wrg/Server.java
@@ -4,15 +4,15 @@ import java.net.URI;
 
 import com.atlas.kafka.consumer.SimpleEventConsumerFactory;
 import com.atlas.shared.rest.RestServerFactory;
-import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
+import com.atlas.wrg.constant.RestConstants;
 import com.atlas.wrg.event.consumer.ChannelServerEventConsumer;
 
 public class Server {
    public static void main(String[] args) {
       SimpleEventConsumerFactory.create(new ChannelServerEventConsumer());
 
-      URI uri = UriBuilder.host(RestService.WORLD_REGISTRY).uri();
+      URI uri = UriBuilder.host(RestConstants.SERVICE).uri();
       RestServerFactory.create(uri, "com.atlas.wrg.rest");
    }
 }
