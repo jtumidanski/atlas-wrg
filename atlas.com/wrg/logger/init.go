734e9f1623b9c0ffea1b62216aace39ec06efdc5
Justin Tumidanski <jtumidanski@gmail.com>
2021-06-14 22:54:31 -0400

    Set logging format to JSON.

--- atlas.com/wrg/logger/init.go
+++ atlas.com/wrg/logger/init.go
@@ -8,6 +8,7 @@ import (
 func CreateLogger() *logrus.Logger {
 	l := logrus.New()
 	l.SetOutput(os.Stdout)
+	l.SetFormatter(&logrus.JSONFormatter{})
 	if val, ok := os.LookupEnv("LOG_LEVEL"); ok {
 		if level, err := logrus.ParseLevel(val); err == nil {
 			l.SetLevel(level)
