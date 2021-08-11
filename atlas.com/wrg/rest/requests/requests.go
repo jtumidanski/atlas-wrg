a00d18b54d82efb027c38f9030705fe42a81800c
Justin Tumidanski <Justin.Tumidanski@dematic.com>
2021-08-11 16:11:33 -0400

    Adjust REST GET function to be fault tolerant (if desired). Migrate topic request to use that common function.

--- atlas.com/wrg/rest/requests/requests.go
+++ atlas.com/wrg/rest/requests/requests.go
@@ -2,8 +2,10 @@ package requests
 
 import (
 	json2 "atlas-wrg/json"
+	"atlas-wrg/retry"
 	"bytes"
 	"encoding/json"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -11,17 +13,46 @@ const (
 	BaseRequest string = "http://atlas-nginx:80"
 )
 
-func get(url string, resp interface{}) error {
-	r, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-
-	err = processResponse(r, resp)
-	return err
+type configuration struct {
+	retries int
 }
 
-func post(url string, input interface{}) (*http.Response, error) {
+type Configurator func(c *configuration)
+
+func SetRetries(amount int) Configurator {
+	return func(c *configuration) {
+		c.retries = amount
+	}
+}
+
+func Get(l logrus.FieldLogger) func(url string, resp interface{}, configurators ...Configurator) error {
+	return func(url string, resp interface{}, configurators ...Configurator) error {
+		c := &configuration{retries: 1}
+		for _, configurator := range configurators {
+			configurator(c)
+		}
+
+		var r *http.Response
+		get := func(attempt int) (bool, error) {
+			var err error
+			r, err = http.Get(url)
+			if err != nil {
+				l.Warnf("Failed calling GET on %s, will retry.", url)
+				return true, err
+			}
+			return false, nil
+		}
+		err := retry.Try(get, c.retries)
+		if err != nil {
+			l.WithError(err).Errorf("Unable to successfully call GET on %s.", url)
+			return err
+		}
+		err = processResponse(r, resp)
+		return err
+	}
+}
+
+func Post(url string, input interface{}) (*http.Response, error) {
 	jsonReq, err := json.Marshal(input)
 	if err != nil {
 		return nil, err
@@ -34,7 +65,7 @@ func post(url string, input interface{}) (*http.Response, error) {
 	return r, nil
 }
 
-func delete(url string) (*http.Response, error) {
+func Delete(url string) (*http.Response, error) {
 	client := &http.Client{}
 	r, err := http.NewRequest(http.MethodDelete, url, nil)
 	if err != nil {
--- atlas.com/wrg/topic/requests.go
+++ atlas.com/wrg/topic/requests.go
@@ -1,12 +1,9 @@
 package topic
 
 import (
-	"atlas-wrg/json"
 	"atlas-wrg/rest/requests"
-	"atlas-wrg/retry"
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
 const (
@@ -17,27 +14,10 @@ const (
 
 func GetTopic(l logrus.FieldLogger) func(topic string) (*dataBody, error) {
 	return func(topic string) (*dataBody, error) {
-		var r *http.Response
-		get := func(attempt int) (bool, error) {
-			var err error
-			r, err = http.Get(fmt.Sprintf(topicById, topic))
-			if err != nil {
-				l.Warningln("Unable to retrieve topic data for %s, will retry.", topic)
-				return true, err
-			}
-			return false, nil
-		}
-
-		err := retry.Try(get, 10)
+		td := &dataContainer{}
+		err := requests.Get(l)(fmt.Sprintf(topicById, topic), requests.SetRetries(10))
 		if err != nil {
 			l.WithError(err).Errorf("Unable to retrieve topic data for %s", topic)
-			return nil, err
-		}
-
-		td := &dataContainer{}
-		err = json.FromJSON(td, r.Body)
-		if err != nil {
-			l.Errorf("Decoding topic data for %s", topic)
 			return nil, err
 		}
 		return &td.Data, nil
