b82119d1c95fdeb05d526f8a2c18c169b25d73ee
Justin Tumidanski <jtumidanski@gmail.com>
2021-08-14 14:10:20 -0400

    Fix issue with topic request.

--- atlas.com/wrg/topic/requests.go
+++ atlas.com/wrg/topic/requests.go
@@ -15,7 +15,7 @@ const (
 func GetTopic(l logrus.FieldLogger) func(topic string) (*dataBody, error) {
 	return func(topic string) (*dataBody, error) {
 		td := &dataContainer{}
-		err := requests.Get(l)(fmt.Sprintf(topicById, topic), requests.SetRetries(10))
+		err := requests.Get(l)(fmt.Sprintf(topicById, topic), td, requests.SetRetries(10))
 		if err != nil {
 			l.WithError(err).Errorf("Unable to retrieve topic data for %s", topic)
 			return nil, err
