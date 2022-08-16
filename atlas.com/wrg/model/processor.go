98dd14a758e3550cb8968402d400b1ab93be1fb8
Justin.Tumidanski@dematic.com <Justin.Tumidanski@dematic.com>
2022-08-16 13:04:29 -0400

    Add more generic model processing functions. This should become part of a library.

--- atlas.com/wrg/model/processor.go
+++ atlas.com/wrg/model/processor.go
@@ -1,6 +1,37 @@
 package model
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+)
+
+type Number interface {
+	uint | uint8 | uint16 | uint32 | uint64
+}
+
+type IdProvider[M Number] func() M
+
+//goland:noinspection GoUnusedExportedFunction
+func FixedIdProvider[M Number](val M) IdProvider[M] {
+	return func() M {
+		return val
+	}
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func ProviderToIdProviderAdapter[M any, N Number](provider Provider[M], transformer Transformer[M, N]) IdProvider[N] {
+	return func() N {
+		m, err := provider()
+		if err != nil {
+			return 0
+		}
+		n, err := transformer(m)
+		if err != nil {
+			return 0
+		}
+		return n
+	}
+}
 
 type Operator[M any] func(M) error
 
@@ -12,6 +43,7 @@ type SliceProvider[M any] func() ([]M, error)
 
 type PreciselyOneFilter[M any] func([]M) (M, error)
 
+//goland:noinspection GoUnusedExportedFunction
 func ExecuteForEach[M any](f Operator[M]) SliceOperator[M] {
 	return func(models []M) error {
 		for _, m := range models {
@@ -26,6 +58,7 @@ func ExecuteForEach[M any](f Operator[M]) SliceOperator[M] {
 
 type Filter[M any] func(M) bool
 
+//goland:noinspection GoUnusedExportedFunction
 func FilteredProvider[M any](provider SliceProvider[M], filters ...Filter[M]) SliceProvider[M] {
 	models, err := provider()
 	if err != nil {
@@ -48,18 +81,21 @@ func FilteredProvider[M any](provider SliceProvider[M], filters ...Filter[M]) Sl
 	return FixedSliceProvider(results)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func FixedProvider[M any](model M) Provider[M] {
 	return func() (M, error) {
 		return model, nil
 	}
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func FixedSliceProvider[M any](models []M) SliceProvider[M] {
 	return func() ([]M, error) {
 		return models, nil
 	}
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func ErrorProvider[M any](err error) Provider[M] {
 	return func() (M, error) {
 		var m M
@@ -67,12 +103,23 @@ func ErrorProvider[M any](err error) Provider[M] {
 	}
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func ErrorSliceProvider[M any](err error) SliceProvider[M] {
 	return func() ([]M, error) {
 		return nil, err
 	}
 }
 
+//goland:noinspection GoUnusedExportedFunction
+func RandomPreciselyOneFilter[M any](ms []M) (M, error) {
+	var def M
+	if len(ms) == 0 {
+		return def, errors.New("empty slice")
+	}
+	return ms[rand.Intn(len(ms))], nil
+}
+
+//goland:noinspection GoUnusedExportedFunction
 func SliceProviderToProviderAdapter[M any](provider SliceProvider[M], preciselyOneFilter PreciselyOneFilter[M]) Provider[M] {
 	return func() (M, error) {
 		ps, err := provider()
@@ -84,6 +131,7 @@ func SliceProviderToProviderAdapter[M any](provider SliceProvider[M], preciselyO
 	}
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func IfPresent[M any](provider Provider[M], operator Operator[M]) {
 	model, err := provider()
 	if err != nil {
@@ -92,6 +140,7 @@ func IfPresent[M any](provider Provider[M], operator Operator[M]) {
 	_ = operator(model)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func For[M any](provider SliceProvider[M], operator SliceOperator[M]) {
 	models, err := provider()
 	if err != nil {
@@ -100,12 +149,15 @@ func For[M any](provider SliceProvider[M], operator SliceOperator[M]) {
 	_ = operator(models)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func ForEach[M any](provider SliceProvider[M], operator Operator[M]) {
 	For(provider, ExecuteForEach(operator))
 }
 
+//goland:noinspection GoUnusedExportedFunction
 type Transformer[M any, N any] func(M) (N, error)
 
+//goland:noinspection GoUnusedExportedFunction
 func Map[M any, N any](provider Provider[M], transformer Transformer[M, N]) Provider[N] {
 	m, err := provider()
 	if err != nil {
@@ -118,6 +170,7 @@ func Map[M any, N any](provider Provider[M], transformer Transformer[M, N]) Prov
 	return FixedProvider(n)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func SliceMap[M any, N any](provider SliceProvider[M], transformer Transformer[M, N]) SliceProvider[N] {
 	models, err := provider()
 	if err != nil {
@@ -135,6 +188,7 @@ func SliceMap[M any, N any](provider SliceProvider[M], transformer Transformer[M
 	return FixedSliceProvider(results)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func First[M any](provider SliceProvider[M], filters ...Filter[M]) (M, error) {
 	var r M
 	ms, err := provider()
